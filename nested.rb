1f6b8f638db7551f298fa215c639d2cae72338f9
Alice Freda <alicefreda@gmail.com>
2019-10-04 22:19:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,10 +73,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+		 programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 		 programmer_hash
-
 
 end
 
